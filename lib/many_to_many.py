5fbce61f75ce8b916cf3f451fd8507fc43378f9b
Rachel Katz <rachel.katz@flatironschool.com>
2023-12-18 12:21:51 -0500

    reset

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,15 +1,6 @@
 class Author:
-    def __init__(self, name):
-        self.name = name 
-
-
+    pass
 class Book:
-    def __init__(self, title):
-        self.title = title 
-
+    pass
 class Contract:
-    def __init__(self, author, book, date, royalties):
-        self.author = author 
-        self.book = book 
-        self.date = date 
-        self.royalties = royalties
+    pass
